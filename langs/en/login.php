662b9c87e7b7a0ce4072b8a96a452d4c010330a1
Bruno Goncalves <bugotech@gmail.com>
2017-06-23 01:00:43 -0300

    login en e es

---
+++ langs/en/login.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'welcome' => 'Sign in',
+    'email' => 'E-mail',
+    'password' => 'Password',
+    'enter' => 'Enter',
+    'remember' => 'Forgot password?',
+];
---
+++ langs/es/login.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'welcome' => 'Iniciar sesión',
+    'email' => 'E-mail',
+    'password' => 'Contraseña',
+    'enter' => 'Enter',
+    'remember' => 'Recordar mi contraseña',
+];
