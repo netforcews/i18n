882d10985dc94d99b3efae603cc098cd166051ee
Bruno Goncalves <bugotech@gmail.com>
2017-06-23 00:31:57 -0300

    login

---
+++ langs/pt-BR/login.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'welcome' => 'Faça o seu login',
+    'email' => 'E-mail',
+    'password' => 'Senha',
+    'enter' => 'Entrar',
+    'remember' => 'Esqueci a minha senha',
+];
