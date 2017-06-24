3f66c395eb660ef60dea57108f35fa5c6291caa3
Bruno Goncalves <bugotech@gmail.com>
2017-06-23 22:33:30 -0300

    auth

---
+++ langs/pt-BR/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'login' => [
+        'welcome' => 'Faça o seu login',
+        'email' => 'E-mail',
+        'password' => 'Senha',
+        'enter' => 'Entrar',
+        'remember' => 'Esqueceu sua senha?',
+    ],
+
+    'remember' => [
+        'welcome' => 'Redefina sua senha',
+        'message' => 'Digite seu endereço de e-mail e nós lhe enviaremos instruções para redefinir a sua senha.',
+        'hint' => 'Digite seu e-mail',
+        'enter' => 'Enviar',
+        'back' => 'Voltar ao login',
+    ],
+];
--- langs/pt-BR/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'welcome' => 'Faça o seu login',
-    'email' => 'E-mail',
-    'password' => 'Senha',
-    'enter' => 'Entrar',
-    'remember' => 'Esqueceu sua senha?',
-];
